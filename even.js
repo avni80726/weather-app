a656675833f49526fd7c5a7cc90ff426416aaf3e
avni <avni80726@gmail.com>
2025-03-08 11:39:09 +0530

    initial commit

---
+++ even.js
@@ -0,0 +1,61 @@
+// function isEvenorOdd(num){
+//     if(num%2==0){
+//         return "Even";
+//     }else{
+//         return "odd";
+//     }
+// }
+// const number = 10;
+// const result = isEvenorOdd(number);
+// console.log(result);
+
+// function findOddAndEven(arr) {
+//   const oddNumber = [];
+//   const evenNumber = [];
+
+//   for (let num of arr) {
+//     if (num % 2 == 0) {
+//       evenNumber.push(num);
+//     } else {
+//       oddNumber.push(num);
+//     }
+//   }
+//   return { oddNumber, evenNumber };
+// }
+// const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const result = findOddAndEven(number);
+
+// console.log("odd number:", result.oddNumber);
+
+// console.log("Even number:", result.evenNumber);
+
+
+// function countEven(arr){
+    
+//     let evenNumber = 0;
+
+//     for (let i=0; i<arr.length; i++){
+//         if(arr[i]%2==0){
+//             evenNumber++;
+//         }
+//     }
+// return {evenNumber};
+    
+// }
+//  const number =[1,2,3,4,5,6,7,8,9,10];
+//  const result = countEven(number)
+//  console.log(result);
+
+
+// function countChar(str){
+//     return str.length;
+
+// }
+// const name = "avnisuhailhusain";
+// const result = countChar(name);
+// console.log(result);
+
+const name ="avnisuhailhusain";
+const result = name.length;
+console.log(result);
+  
